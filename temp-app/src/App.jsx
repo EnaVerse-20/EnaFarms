96e2ea6ff5be7228a81a7f3c53fcb6d3662e9f9f
Prerna17-05 <prernadongredongre@gmail.com>
2026-06-07 15:58:15 +0530

    Created project structure and navbar

--- temp-app/src/App.jsx
+++ temp-app/src/App.jsx
@@ -1,10 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-green-100 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-green-700">
-        EnaFarms
-      </h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ temp-app/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+  return (
+    <nav className="bg-green-700 text-white shadow-md">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        
+        <div>
+          <h1 className="text-2xl font-bold">EnaFarms</h1>
+          <p className="text-xs">Tech Behind Every Harvest</p>
+        </div>
+
+        <ul className="flex gap-6 font-medium">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Market</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
